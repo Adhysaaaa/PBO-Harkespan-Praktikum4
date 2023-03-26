81a72f04770f77ddc041f3a8f251859083a5a86e
Adhysaaaa <126860766+Adhysaaaa@users.noreply.github.com>
2023-03-26 13:52:42 +0700

    Add files via upload

---
+++ rumusabc.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class rumusabc {
+    public static void main(String args[]){
+            
+    Scanner input = new Scanner(System.in);
+        
+    int a, b, c, D;
+    double x1, x2;
+        
+    System.out.println("Format persamaan: ax^2 + bx + c = 0 ");
+    System.out.print("Input nilai a: ");
+    a = input.nextInt();
+    System.out.print("Input nilai b: ");
+    b = input.nextInt();
+    System.out.print("Input nilai c: ");
+    c = input.nextInt();
+        
+    System.out.println();
+        
+    D = (b*b)-(4*a*c);
+    System.out.print("Diskriminan = "+D);
+        
+    if (D>0){
+        System.out.println(" (akar real dan berbeda)");
+    
+        x1 = (-b + Math.sqrt(D)) / (2*a);
+        x2 = (-b - Math.sqrt(D)) / (2*a);
+        
+        System.out.println("x1 = "+x1);
+        System.out.println("x2 = "+x2);
+    }
+    else if (D==0){
+        System.out.println(" (akar real dan sama)");
+    
+        x1 = x2 = (-b + Math.sqrt(D)) / (2*a);
+        
+        System.out.println("x1 = "+x1);
+        System.out.println("x2 = "+x2);
+    }
+    else {
+        System.out.println(" (akar tidak real / imajiner)");
+    }  
+    
+    }
+}
